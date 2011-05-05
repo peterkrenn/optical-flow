0dc5021a5a2cd5c109a2262679454513e43e379d
Peter Krenn <peter.krenn@gmail.com>
2011-05-05 17:34:23 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem 'sqlite3'
+gem 'decent_exposure'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'fuubar'
+  gem 'shoulda-matchers'
+  gem 'factory_girl_rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpticalFlow::Application.config.secret_token = 'db74b9069a84a6fdf71c905f4b5f910ccdd4f878d2f5981a05ab4d0296d4e35772cab57843102f325954ffc1983b426f9919bed2a2c95aac965d8e399501f9d0'
