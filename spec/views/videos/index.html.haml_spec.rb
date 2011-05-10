463c2041d7ccf84a44d48ae16fc7c028716e45e8
Peter Krenn <peter.krenn@gmail.com>
2011-05-10 19:31:59 +0200

    Update videos/index spec to handle a lack of videos

--- spec/views/videos/index.html.haml_spec.rb
+++ spec/views/videos/index.html.haml_spec.rb
@@ -1,13 +1,23 @@
 require 'spec_helper'
 
 describe 'videos/index.html.haml' do
-  let(:videos) { [double('Video', :file_name => 'file_name')] }
+  context 'with a video' do
+    let(:videos) { [double('Video', :file_name => 'file_name')] }
 
-  it 'displays all videos' do
-    view.should_receive(:videos).once.and_return(videos)
+    it 'displays all videos' do
+      view.should_receive(:videos).once.and_return(videos)
+      render
+      rendered.should have_selector 'li', :count => 1
+    end
+  end
 
-    render
+  context 'without videos' do
+    let(:videos) { [] }
 
-    rendered.should have_selector 'li', :count => 1
+    it "doesn't display videos" do
+      view.should_receive(:videos).once.and_return(videos)
+      render
+      rendered.should_not have_selector 'li'
+    end
   end
 end
