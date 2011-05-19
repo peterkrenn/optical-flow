488f9f05998df4396ae2268df83ed470c9c8ac2f
Peter Krenn <peter.krenn@gmail.com>
2011-05-19 16:33:45 +0200

    Add workers#index

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,2 +1,4 @@
 class WorkersController < ApplicationController
+  def index
+  end
 end
--- spec/controllers/workers_controller_spec.rb
+++ spec/controllers/workers_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe WorkersController do
+  describe 'GET index' do
+    it 'is successful' do
+      get :index
+      response.should be_successful
+    end
 
+    it 'renders' do
+      get :index
+      response.should render_template 'index'
+    end
+  end
 end
