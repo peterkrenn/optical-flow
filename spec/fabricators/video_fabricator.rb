4d0c311046e074d0ba72ba5789e9ff5021be6f90
Peter Krenn <peter.krenn@gmail.com>
2011-11-21 18:08:02 +0100

    Update display of videos for states

--- spec/fabricators/video_fabricator.rb
+++ spec/fabricators/video_fabricator.rb
@@ -1,4 +1,20 @@
 Fabricator(:video) do
-  original_video { File.open(Rails.root.join('app', 'assets', 'images', 'rails.png')) }
+  original_video File.open(Rails.root.join('app', 'assets', 'images', 'rails.png'))
   original_filename 'rails.png'
+end
+
+Fabricator(:enqueued_video, :from => :video) do
+  state 'enqueued'
+  enqueued_at Time.now
+end
+
+Fabricator(:processing_video, :from => :enqueued_video) do
+  state 'processing'
+  processing_started_at Time.now
+end
+
+Fabricator(:processed_video, :from => :processing_video) do
+  processed_video File.open(Rails.root.join('app', 'assets', 'images', 'rails.png'))
+  state 'processed'
+  processing_finished_at Time.new
 end
--- spec/views/videos/index.html.haml_spec.rb
+++ spec/views/videos/index.html.haml_spec.rb
@@ -2,21 +2,79 @@ require 'spec_helper'
 
 describe 'videos/index.html.haml' do
   context 'with videos' do
-    let(:videos) { [stub_model(Video, :original_filename => 'video.mov')] * 2 }
+    let(:videos) { [Fabricate(:video), Fabricate(:enqueued_video), Fabricate(:processing_video), Fabricate(:processed_video)] }
 
     before(:each) { view.should_receive(:videos).once.and_return(videos) }
 
     it 'displays all videos' do
       render
       rendered.should have_selector '#video_list ul' do |ul|
-        ul.should have_selector 'li', :content => 'video.mov', :count => 2
+        ul.should have_selector 'li', :count => 4
       end
     end
 
-    it 'displays process links for all videos' do
+    it 'displays the original filename for all videos' do
+      render
+      rendered.should have_selector 'p', :content => 'rails.png', :count => 4
+    end
+
+    it 'displays a process link for unprocessed videos' do
       render
       videos.each do |video|
-        rendered.should have_selector 'a', :href => "/videos/#{video.id}", :'data-method' => 'put'
+        if video.unprocessed?
+          rendered.should have_selector 'p' do |p|
+            p.should have_selector 'a', :href => "/videos/#{video.id}", :'data-method' => 'put'
+          end
+        else
+          rendered.should_not have_selector 'a', :href => "/videos/#{video.id}", :'data-method' => 'put'
+        end
+      end
+    end
+
+    it 'displays download link for the original video for all videos' do
+      render
+      videos.each do |video|
+        rendered.should have_selector 'p' do |p|
+          p.should have_selector 'a', :href => video.original_video.url, :content => 'download original file'
+        end
+      end
+    end
+
+    it 'displays the time spent in queue for enqueued videos' do
+      render
+      videos.each do |video|
+        if video.enqueued?
+          rendered.should have_selector 'p', :content => "enqueued for #{time_ago_in_words(video.enqueued_at)}"
+        end
+      end
+    end
+
+    it 'displays the time spent processing for processing videos' do
+      render
+      videos.each do |video|
+        if video.processing?
+          rendered.should have_selector 'p', :content => "processing since #{time_ago_in_words(video.processing_started_at)}"
+        end
+      end
+    end
+
+    it 'displays the processing time for processed videos' do
+      render
+      videos.each do |video|
+        if video.processed?
+          rendered.should have_selector :p, :content => "processed in #{distance_of_time_in_words(video.processing_finished_at, video.processing_started_at, true)}"
+        end
+      end
+    end
+
+    it 'displays download link for the processed video for processed videos' do
+      render
+      videos.each do |video|
+        if video.processed?
+          rendered.should have_selector 'p' do |p|
+            p.should have_selector 'a', :href => video.processed_video.url, :content => 'download processed file'
+          end
+        end
       end
     end
   end
